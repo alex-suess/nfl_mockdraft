7251503401189864dff8682cafa92b6eef669874
superkicker31 <alexandersuess@hotmail.de>
2017-03-22 14:49:55 +0100

    Basic Laravel installation

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,24 @@
+
+
+<?php
+use App\User;
+use App\Draftpick;
+use App\Draftboard;
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(User::class, function (Faker\Generator $faker) {
+    return [
+        'username' => $faker->username,
+        'remember_token' => str_random(10),
+    ];
+});
